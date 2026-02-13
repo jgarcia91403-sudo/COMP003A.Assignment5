83e63b06dfc2c282f21ee234f6f02b33f5d097bc
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-02-13 13:44:55 -0800

    Was able to get some of the beggining down have to fix the red lines at the end.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ShowIntroduction();
+            string repeat = "y";
+            while (repeat == "y")
+            {
+                Console.Write(" Enter your name:");
+                string name = Console.ReadLine();
+                Console.Write(" Enter a whole number:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                DisplayUserInfo(name, number);
+                int result = CalculativeValue(number);
+                Console.WriteLine(" Calculated result:" + result);
+            }
+
         }
     }
 }
